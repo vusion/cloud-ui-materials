006f5fc33848e3ad5b0e3ed6d1e939431adb1881
Rainfore <rainforest92@126.com>
2019-12-13 03:53:46 +0800

    :wrench: Fix package.json

--- vusion.config.js
+++ vusion.config.js
@@ -1,18 +1,5 @@
 module.exports = {
     version: '>=0.8.2',
-    type: 'library',
+    type: 'repository',
     name: 'cloud-materials',
-    CamelName: 'CloudMaterials',
-    docs: {
-        title: 'Cloud 物料仓库',
-        logo: '物料仓库',
-        base: '/cloud-materials/',
-        github: 'https://github.com/vusion/cloud-materials#next',
-        navbar: [
-            { text: '区块', to: '/blocks' },
-        ],
-        blocks: [
-            { group: 'button', name: '', alias: '按钮' },
-        ],
-    },
 };
