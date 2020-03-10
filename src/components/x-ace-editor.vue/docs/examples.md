45f87c2e1b5fec9ac9e8e26daf57e22116a42061
Forrest <rainforest92@126.com>
2020-03-10 20:34:51 +0800

    :memo: Add mode & theme demo

--- src/components/x-ace-editor.vue/docs/examples.md
+++ src/components/x-ace-editor.vue/docs/examples.md
@@ -1,10 +1,32 @@
 ### 基本用法
 
-``` html
-<x-ace-editor></x-ace-editor>
+使用`v-model`或`:value.sync`双向绑定。
+
+``` vue
+<template>
+<x-ace-editor v-model="value"></x-ace-editor>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            value: 'Ace Editor',
+        };
+    },
+}
+</script>
 ```
 
 ### 设置语言
+
+通过`lang`属性设置语言。更多语言包请查看 [brace mode](https://github.com/thlorenz/brace/tree/master/mode)。
+
+注意：需要用如下方式引入 brace 包中的语言包。
+
+``` js
+import 'brace';
+import 'brace/mode/json';
+```
 
 ``` vue
 <template>
@@ -30,7 +52,56 @@ export default {
 </script>
 ```
 
+下面再多举一些例子：
+
+``` vue
+<template>
+<u-linear-layout direction="vertical">
+    <u-form-item label="切换语言：">
+        <u-select v-model="selectedLang">
+            <u-select-item v-for="lang in langs" :key="lang" :value="lang">{{ lang }}</u-select-item>
+        </u-select>
+    </u-form-item>
+    <x-ace-editor v-model="values[selectedLang]" :lang="selectedLang"></x-ace-editor>
+</u-linear-layout>
+</template>
+<script>
+import 'brace';
+import 'brace/mode/json';
+import 'brace/mode/python';
+import 'brace/mode/sql';
+
+export default {
+    data() {
+        return {
+            selectedLang: 'json',
+            values: {
+                json: `{
+    "name": "Alice",
+    "age": 24,
+    "phone": "18612345678"
+}
+`,
+                python: `def factorial(n):
+    return reduce(lambda x,y:x*y,[1]+range(1,n+1))`,
+                sql: 'SELECT * FROM articles WHERE user_id = "123abc";',
+            },
+        };
+    },
+    computed: {
+        langs() {
+            return Object.keys(this.values);
+        },
+    },
+}
+</script>
+```
+
 ### 设置主题
+
+通过`theme`属性设置常见的编辑器主题。更多主题请查看 [brace theme](https://github.com/thlorenz/brace/tree/master/theme)。
+
+注意：同样需要提前引入 brace 包中的主题文件。
 
 ``` vue
 <template>
@@ -57,14 +128,51 @@ export default {
 </script>
 ```
 
-### 禁用
+下面再多举一些例子：
 
-``` html
-<x-ace-editor value="var i = 0;" disabled></x-ace-editor>
+``` vue
+<template>
+<u-linear-layout direction="vertical">
+    <u-form-item label="切换主题：">
+        <u-select v-model="selectedTheme">
+            <u-select-item v-for="theme in themes" :key="theme" :value="theme">{{ theme }}</u-select-item>
+        </u-select>
+    </u-form-item>
+    <x-ace-editor v-model="value" lang="javascript" :theme="selectedTheme"></x-ace-editor>
+</u-linear-layout>
+</template>
+<script>
+import 'brace';
+import 'brace/mode/javascript';
+
+const themes = ['ambiance','chaos','chrome','clouds','clouds_midnight','cobalt','crimson_editor','dawn','dracula','dreamweaver','eclipse','github','gob','gruvbox','idle_fingers','iplastic','katzenmilch','kr_theme','kuroir','merbivore','merbivore_soft','mono_industrial','monokai','pastel_on_dark','solarized_dark','solarized_light','sqlserver','terminal','textmate','tomorrow','tomorrow_night','tomorrow_night_blue','tomorrow_night_bright','tomorrow_night_eighties','twilight','vibrant_ink','xcode'];
+themes.map((theme) => import(`brace/theme/${theme}`));
+
+export default {
+    data() {
+        return {
+            selectedTheme: '',
+            themes,
+            value: `[1, 2, 3].map((x) => {
+    const y = x + 1;
+    return x * y;
+});
+`,
+        };
+    },
+}
+</script>
 ```
 
-### 只读
+### 只读和禁用
 
 ``` html
-<x-ace-editor value="var i = 0;" readonly></x-ace-editor>
+<u-grid-layout :repeat="2">
+    <u-grid-layout-column>
+        <x-ace-editor value="var i = 0; // 只读" readonly></x-ace-editor>
+    </u-grid-layout-column>
+    <u-grid-layout-column>
+        <x-ace-editor value="var i = 0; // 禁用" disabled></x-ace-editor>
+    </u-grid-layout-column>
+</u-grid-layout>
 ```
--- src/components/x-ace-editor.vue/index.js
+++ src/components/x-ace-editor.vue/index.js
@@ -1,9 +1,15 @@
-import XAceEditor from 'x-ace-editor.vue';
+import { MField } from 'cloud-ui.vusion';
+import * as ace from 'brace';
 
-export default {
+export const XAceEditor = {
     name: 'x-ace-editor',
-    extends: XAceEditor,
+    mixins: [MField],
     props: {
+        value: { type: String, default: '' },
+        lang: { type: String, default: 'text' },
+        theme: { type: String },
+        readonly: { type: Boolean, default: false },
+        disabled: { type: Boolean, default: false },
         options: {
             type: Object,
             default() {
@@ -12,9 +18,143 @@ export default {
                 };
             },
         },
-        themeMode: {
-            type: String,
-            default: 'no-follow',
+        autofocus: { type: Boolean, default: false },
+    },
+    data() {
+        // 就不监听了
+        this.editor = undefined;
+
+        return {
+            currentValue: '',
+        };
+    },
+    watch: {
+        lang(lang) {
+            this.watchLang(lang);
+        },
+        theme(theme) {
+            this.watchTheme(theme);
+        },
+        value(value) {
+            this.watchValue(value);
+        },
+        readonly(readonly) {
+            this.watchReadonly(readonly);
+        },
+        disabled(disabled) {
+            this.watchDisabled(disabled);
+        },
+        options(options) {
+            this.editor && this.editor.setOptions(options);
         },
     },
+    mounted() {
+        this.editor = ace.edit(this.$el);
+        const options = this.options;
+        if (options)
+            this.editor.setOptions(options);
+
+        if (this.autofocus)
+            this.focusLastLine();
+
+        this.watchLang(this.lang);
+        this.watchTheme(this.theme);
+        this.watchValue(this.value);
+        this.watchDisabled(this.disabled);
+        this.watchReadonly(this.disabled || (options && options.readOnly) || this.readonly);
+
+        // @question
+        this.editor.$blockScrolling = Infinity;
+
+        this.editor.on('change', (e) => {
+            const value = this.editor.getValue();
+            this.$emit('input', value);
+            this.$emit('update:value', value);
+            const oldValue = this.currentValue;
+            this.currentValue = value;
+
+            let valid = true;
+            if (this.lang === 'json')
+                valid = this.validateJSON(value);
+            this.$emit('validate', {
+                valid,
+            });
+
+            this.$emit('change', {
+                value,
+                oldValue,
+            });
+        });
+
+        // 其他事件直接透传
+        ['blur', 'changeSelectionStyle', 'changeSession', 'copy', 'focus', 'paste'].forEach((event) => {
+            this.editor.on(event, (e) => this.$emit(event, e));
+        });
+
+        // @TODO: 方法要不要透传
+
+        this.$emit('init', {
+            editor: this.editor,
+        });
+    },
+    methods: {
+        watchValue(value) {
+            if (this.currentValue === value)
+                return;
+
+            this.editor.setValue(value, 1);
+            this.currentValue = value;
+        },
+        watchLang(lang) {
+            this.editor.getSession().setMode(`ace/mode/${lang}`);
+        },
+        watchTheme(theme) {
+            if (theme)
+                this.editor.setTheme(`ace/theme/${theme}`);
+        },
+        watchReadonly(readonly) {
+            this.editor.setOptions({
+                readOnly: readonly,
+            });
+        },
+        watchDisabled(disabled) {
+            if (disabled) {
+                this.editor.setOptions({
+                    readOnly: true,
+                    highlightActiveLine: false,
+                    highlightGutterLine: false,
+                });
+                this.editor.renderer.$cursorLayer.element.style.opacity = 0;
+            } else {
+                this.editor.setOptions({
+                    readOnly: false,
+                    highlightActiveLine: true,
+                    highlightGutterLine: true,
+                });
+                this.editor.renderer.$cursorLayer.element.style.opacity = '';
+            }
+        },
+        validateJSON(json) {
+            if (!json)
+                return true;
+
+            try {
+                JSON.parse(json);
+            } catch (error) {
+                return false;
+            }
+            return true;
+        },
+        focusLastLine() {
+            this.editor.focus();
+            const session = this.editor.getSession();
+            const count = session.getLength();
+            this.editor.gotoLine(count, session.getLine(count - 1).length);
+        },
+    },
+    destroyed() {
+        this.editor.destroy();
+    },
 };
+
+export default XAceEditor;
