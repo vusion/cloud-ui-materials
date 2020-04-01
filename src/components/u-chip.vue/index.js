0752920cba49e8c3bf884c87814d6dbc38c376e4
Forrest <rainforest92@126.com>
2020-04-01 18:41:41 +0800

    :bookmark: Publish @cloud-ui/u-chip.vue@0.1.4

--- src/components/u-chip.vue/index.js
+++ src/components/u-chip.vue/index.js
@@ -1,6 +1,6 @@
 import { UValidator } from 'cloud-ui.vusion';
 
-export default {
+export const UChip = {
     name: 'u-chip',
     mixins: [UValidator],
     props: {
@@ -17,3 +17,5 @@ export default {
         },
     },
 };
+
+export default UChip;
