04c2cbed449f5f483b3a7db84058ed0797e8703e
Forrest <rainforest92@126.com>
2020-04-09 00:50:15 +0800

    :bug: Add layout startTime

--- src/components/u-relations-diagram.vue/index.js
+++ src/components/u-relations-diagram.vue/index.js
@@ -237,7 +237,7 @@ export const URelationsDiagram = {
                 condense: true,
                 padding: 0,
             }).run();
-        });
+        }, 60);
     },
     methods: {
         handleData(data) {
