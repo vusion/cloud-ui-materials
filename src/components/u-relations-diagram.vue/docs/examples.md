377723f0d25506fc5d529dc930e953e117235570
Forrest <rainforest92@126.com>
2020-04-08 23:53:05 +0800

    :sparkles: Complete first version

---
+++ src/components/u-relations-diagram.vue/docs/examples.md
@@ -0,0 +1,164 @@
+### 基本用法
+
+在`data`属性中传入`nodes`和`edges`的数据。
+
+使用`layout`属性设置布局，布局方案详见 [Cytoscape.js Layouts](https://js.cytoscape.org/#layouts)。
+
+对原始 Cytoscape.js 节点格式做了简化：
+
+``` ts
+interface node {
+    id: string, // 节点 id 和名称
+    parent: string, // 组合方式中的父节点
+    color: string, // 配色
+};
+interface edge {
+    id: string, // 节点 id 和名称
+    source: string, // 起始节点
+    target: string, // 目标节点
+    color: string, // 配色
+};
+```
+
+``` html
+<u-relations-diagram :data="{
+    nodes: [
+        { id: 'n0', color: 'primary' },
+        { id: 'n1' },
+        { id: 'n2', color: 'warning' },
+        { id: 'n3' },
+        { id: 'n4' },
+    ],
+    edges: [
+        { source: 'n0', target: 'n1' },
+        { source: 'n1', target: 'n2' },
+        { source: 'n0', target: 'n3' },
+        { source: 'n1', target: 'n3' },
+        { source: 'n1', target: 'n4' },
+        { source: 'n2', target: 'n4' },
+    ],
+}" :layout="{
+    name: 'circle',
+}"></u-relations-diagram>
+```
+
+### 图例与配色
+
+使用`legend`属性添加图例。
+
+``` html
+<u-relations-diagram :data="{
+    nodes: [
+        { id: 'default' },
+        { id: 'primary', color: 'primary' },
+        { id: 'success', color: 'success' },
+        { id: 'normal', color: 'normal' },
+        { id: 'problem', color: 'problem' },
+        { id: 'warning', color: 'warning' },
+        { id: 'error', color: 'error' },
+        { id: 'danger', color: 'danger' },
+        { id: 'disabled', color: 'disabled' },
+    ],
+    edges: [
+        { source: 'default', target: 'primary' },
+        { source: 'primary', target: 'success', color: 'primary' },
+        { source: 'success', target: 'normal', color: 'success' },
+        { source: 'normal', target: 'problem', color: 'normal' },
+        { source: 'problem', target: 'warning', color: 'problem' },
+        { source: 'warning', target: 'error', color: 'warning' },
+        { source: 'error', target: 'danger', color: 'error' },
+        { source: 'danger', target: 'disabled', color: 'danger' },
+        { source: 'disabled', target: 'default', color: 'disabled' },
+    ],
+}" :layout="{
+    name: 'circle',
+}"></u-relations-diagram>
+```
+
+### 自定义样式
+
+使用`element-style`属性自定义样式。样式设置详见 [Cytoscape.js Style](https://js.cytoscape.org/#style)。
+
+``` html
+<u-relations-diagram :data="{
+    nodes: [
+        { id: '建德私有云' },
+        { id: '滨江私有云', color: 'success' },
+        { id: '萧山私有云', color: 'success' },
+    ],
+    edges: [
+        { source: '建德私有云', target: '滨江私有云', color: 'primary' },
+        { source: '滨江私有云', target: '萧山私有云', color: 'primary' },
+        { source: '萧山私有云', target: '建德私有云', color: 'primary' },
+    ],
+}" :legend="[
+    { label: '评估完成', color: 'success' },
+    { label: '评估中', color: 'default' },
+]" :layout="{
+    name: 'circle',
+}" :element-style="[{
+    selector: 'node',
+    css: {
+        padding: 20,
+    },
+}]"></u-relations-diagram>
+```
+
+### 复杂示例
+
+下面是一个更复杂的示例，使用了[cose Layout](https://js.cytoscape.org/#layouts/cose)。
+
+``` vue
+<template>
+<u-relations-diagram :data="data" :legend="[
+    { label: '正常', color: 'normal' },
+    { label: '风险', color: 'danger' },
+]" :layout="{
+    name: 'cose',
+    animate: false,
+    componentSpacing: 80,
+}"></u-relations-diagram>
+</template>
+<script>
+export default {
+    data() {
+        const data = {
+            nodes: [
+                { id: 'RDS' },
+                { id: 'K8S' },
+                { id: 'DNS' },
+                { id: 'nova' },
+                { id: 'CEPH' },
+                { id: 'VPC' },
+                { id: 'rds-on-k8s-with-floating-ip', parent: 'RDS', color: 'danger' },
+                { id: 'k8s-node', parent: 'K8S', color: 'normal' },
+                { id: 'k8s-volume', parent: 'K8S', color: 'danger' },
+                { id: 'dns', parent: 'DNS', color: 'normal' },
+                { id: 'nova-vm', parent: 'nova', color: 'normal' },
+                { id: 'ceph-ssd-volume', parent: 'CEPH', color: 'danger' },
+                { id: 'vpc-floating-ip', parent: 'VPC', color: 'normal' },
+            ],
+            edges: [
+                { id: 'rds-k8s-node', source: 'rds-on-k8s-with-floating-ip', target: 'k8s-node' },
+                { id: 'rds-k8s-volume', source: 'rds-on-k8s-with-floating-ip', target: 'k8s-volume' },
+                { source: 'rds-on-k8s-with-floating-ip', target: 'vpc-floating-ip' },
+                { source: 'k8s-node', target: 'dns' },
+                { source: 'k8s-node', target: 'nova-vm' },
+                { source: 'k8s-volume', target: 'ceph-ssd-volume' },
+                { source: 'ceph-ssd-volume', target: 'nova-vm' },
+                { source: 'dns', target: 'vpc-floating-ip' },
+            ],
+        };
+
+        // layout 使用 cose 的话，最好初始把节点拉开，否则每次位置都是随机的。
+        data.nodes.forEach((node, index) => {
+            node.position = { x: index * 60, y: index * 60 };
+        });
+        return {
+            data,
+        };
+    },
+}
+</script>
+```
+
---
+++ src/components/u-relations-diagram.vue/index.js
@@ -0,0 +1,309 @@
+import Vue from 'vue';
+import VueCytoscape from 'vue-cytoscape';
+Vue.use(VueCytoscape);
+
+import colors from './colors';
+const style = [
+    {
+        selector: 'node',
+        css: {
+            label: 'data(id)',
+            shape: 'rectangle',
+            width: 'label',
+            height: 'label',
+            padding: 8,
+            color: colors['color-base'],
+            'font-size': 14,
+            'background-color': colors['background-color-base'],
+            'border-width': 2,
+            'border-color': colors['border-color-base'],
+            'text-valign': 'center',
+            'text-halign': 'center',
+            // 'compound-sizing-wrt-labels': 'include',
+        },
+    },
+    {
+        selector: 'node[color="primary"]',
+        css: {
+            color: 'white',
+            'background-color': colors['brand-primary'],
+            'border-color': colors['brand-primary'],
+        },
+    },
+    {
+        selector: 'node[color="success"]',
+        css: {
+            color: 'white',
+            'background-color': colors['brand-success'],
+            'border-color': colors['brand-success'],
+        },
+    },
+    {
+        selector: 'node[color="normal"]',
+        css: {
+            color: 'white',
+            'background-color': colors['brand-normal'],
+            'border-color': colors['brand-normal'],
+        },
+    },
+    {
+        selector: 'node[color="problem"]',
+        css: {
+            color: 'white',
+            'background-color': colors['brand-problem'],
+            'border-color': colors['brand-problem'],
+        },
+    },
+    {
+        selector: 'node[color="warning"]',
+        css: {
+            color: 'white',
+            'background-color': colors['brand-warning'],
+            'border-color': colors['brand-warning'],
+        },
+    },
+    {
+        selector: 'node[color="error"]',
+        css: {
+            color: 'white',
+            'background-color': colors['brand-error'],
+            'border-color': colors['brand-error'],
+        },
+    },
+    {
+        selector: 'node[color="danger"]',
+        css: {
+            color: 'white',
+            'background-color': colors['brand-error'],
+            'border-color': colors['brand-error'],
+        },
+    },
+    {
+        selector: 'node[color="disabled"]',
+        css: {
+            color: 'white',
+            'background-color': colors['brand-disabled'],
+            'border-color': colors['brand-disabled'],
+        },
+    },
+    {
+        selector: ':parent',
+        css: {
+            'text-valign': 'top',
+            'text-halign': 'center',
+            'background-opacity': 0,
+        },
+    },
+    {
+        selector: 'edge',
+        css: {
+            color: colors['color-base'],
+            'font-size': 14,
+            width: 2,
+            'line-color': colors['color-lighter'],
+            'target-arrow-shape': 'triangle',
+            'target-arrow-color': colors['color-lighter'],
+        },
+    },
+    {
+        selector: 'edge[color="primary"]',
+        css: {
+            'line-color': colors['brand-primary'],
+        },
+    },
+    {
+        selector: 'edge[color="success"]',
+        css: {
+            'line-color': colors['brand-success'],
+        },
+    },
+    {
+        selector: 'edge[color="normal"]',
+        css: {
+            'line-color': colors['brand-normal'],
+        },
+    },
+    {
+        selector: 'edge[color="problem"]',
+        css: {
+            'line-color': colors['brand-problem'],
+        },
+    },
+    {
+        selector: 'edge[color="warning"]',
+        css: {
+            'line-color': colors['brand-warning'],
+        },
+    },
+    {
+        selector: 'edge[color="error"]',
+        css: {
+            'line-color': colors['brand-error'],
+        },
+    },
+    {
+        selector: 'edge[color="danger"]',
+        css: {
+            'line-color': colors['brand-error'],
+        },
+    },
+    {
+        selector: 'edge[color="disabled"]',
+        css: {
+            'line-color': colors['brand-disabled'],
+        },
+    },
+];
+
+export const URelationsDiagram = {
+    name: 'u-relations-diagram',
+    props: {
+        /**
+         * type node = { id: string, label: string, parent: string, color: string }
+         * type edge = { id: string, source: string, target: string, color: string }
+         *
+         * type data = {
+         *  nodes: Array<node>,
+         *  edges: Array<edge>,
+         * }
+         */
+        data: Object,
+        legend: Array,
+        layout: { type: [String, Object], default: 'grid' },
+        elementStyle: { type: Array, default() { return []; } },
+        config: Object,
+    },
+    data() {
+        return {
+            elements: this.handleData(this.data),
+            currentConfig: Object.assign({
+                userZoomingEnabled: false,
+                style: [
+                    ...style,
+                    ...this.elementStyle,
+                ],
+                minZoom: 0.5,
+                maxZoom: 2,
+            }, this.config),
+            currentLayout: this.handleLayout(this.layout),
+            legendConfig: {
+                zoomingEnabled: false,
+                panningEnabled: false,
+                boxSelectionEnabled: false,
+                autoungrabify: true,
+                // autolock: true,
+                autounselectify: true,
+                style: [
+                    ...style,
+                    ...this.elementStyle,
+                    {
+                        selector: 'node',
+                        css: {
+                            label: 'data(label)',
+                            width: 'label',
+                            height: '2',
+                            'font-size': 12,
+                            padding: 10,
+                        },
+                    },
+                ],
+                minZoom: 0.5,
+                maxZoom: 2,
+            },
+            legendElements: this.handleLegend(this.legend),
+        };
+    },
+    watch: {
+        data(data) {
+            this.elements = this.handleData(data);
+        },
+        layout(layout) {
+            this.currentLayout = this.handleLayout(layout);
+            this.runLayout();
+        },
+    },
+    mounted() {
+        // 经测试，必须要加 setTimeout
+        setTimeout(() => {
+            this.runLayout();
+
+            if (!this.legend)
+                return;
+            const legend = this.$refs.legend.instance;
+            legend.layout({
+                fit: false,
+                name: 'grid',
+                rows: 1,
+                condense: true,
+                padding: 0,
+            }).run();
+        });
+    },
+    methods: {
+        handleData(data) {
+            if (!data) {
+                return [];
+            } else {
+                const KEYWORDS = ['position', 'selected', 'selectable', 'locked', 'grabbable', 'pannable'];
+
+                const elements = [];
+                data.nodes && data.nodes.forEach((node, index) => {
+                    const el = Object.assign({}, node);
+                    const elData = Object.assign({}, node);
+
+                    Object.keys(el).forEach((key) => {
+                        if (KEYWORDS.includes(key))
+                            delete elData[key];
+                        else
+                            delete el[key];
+                    });
+
+                    el.data = elData;
+                    elements.push(el);
+                });
+                data.edges && data.edges.forEach((node) => {
+                    const el = Object.assign({
+                        selectable: false,
+                    }, node);
+                    const elData = Object.assign({}, node);
+
+                    Object.keys(el).forEach((key) => {
+                        if (KEYWORDS.includes(key))
+                            delete elData[key];
+                        else
+                            delete el[key];
+                    });
+
+                    el.data = elData;
+                    elements.push(el);
+                });
+
+                return elements;
+            }
+        },
+        handleLayout(layout) {
+            if (typeof layout === 'string')
+                return { name: layout };
+            else
+                return layout;
+        },
+        runLayout() {
+            const cy = this.$refs.cy.instance;
+            const layout = cy.layout(this.currentLayout);
+            layout.run();
+        },
+        handleLegend(legend) {
+            const elements = [];
+            legend && legend.forEach((node, index) => {
+                const el = {
+                    data: node,
+                };
+                elements.push(el);
+            });
+
+            return elements;
+        },
+    },
+};
+
+export default URelationsDiagram;
