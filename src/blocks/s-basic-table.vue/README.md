7a445dbc821aefed930b1dce295c6c4157d28dd4
Forrest <rainforest92@126.com>
2020-06-29 14:40:47 +0800

    :bookmark: Publish @cloud-ui/s-basic-table.vue@0.2.0

--- src/blocks/s-basic-table.vue/README.md
+++ src/blocks/s-basic-table.vue/README.md
@@ -1,5 +1,7 @@
 # SBasicTable 基础表格
 
+请在这里添加描述
+
 ``` html
 <s-basic-table></s-basic-table>
 ```
---
+++ src/blocks/s-basic-table.vue/index.vue
@@ -0,0 +1,40 @@
+<template>
+<u-linear-layout direction="vertical">
+    <u-linear-layout>
+        列表页。
+    </u-linear-layout>
+    <u-linear-layout>
+        <u-button color="primary" icon="create" to="create">创建</u-button>
+        <u-button square icon="refresh" @click="$refs.tableView.reload()"></u-button>
+    </u-linear-layout>
+    <u-table-view ref="tableView" :data-source="load">
+        <u-table-view-column type="checkbox" width="8%"></u-table-view-column>
+        <u-table-view-column title="名称" field="name"></u-table-view-column>
+        <u-table-view-column title="操作">
+            <template #cell="{ item }">
+                <u-linear-layout gap="small">
+                    <u-link :to="{ path: 'detail', query: { id: item.id } }">
+                        查看详情
+                    </u-link>
+                </u-linear-layout>
+            </template>
+        </u-table-view-column>
+    </u-table-view>
+</u-linear-layout>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    methods: {
+        load() {
+            return this.$services.service_PLACEHOLDER.loadList_PLACEHOLDER();
+        },
+    },
+};
+</script>
+<style module>
+.root {}
+</style>
