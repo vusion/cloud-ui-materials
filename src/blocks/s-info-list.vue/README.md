9d9bbdcfb29996b1ef737e549afb37960a38c9a6
Forrest <rainforest92@126.com>
2020-06-29 15:07:10 +0800

    :bookmark: Publish @cloud-ui/s-info-list.vue@0.2.0

--- src/blocks/s-info-list.vue/README.md
+++ src/blocks/s-info-list.vue/README.md
@@ -1,5 +1,7 @@
 # SInfoList 信息列表
 
+请在这里添加描述
+
 ``` html
 <s-info-list></s-info-list>
 ```
---
+++ src/blocks/s-info-list.vue/index.vue
@@ -0,0 +1,22 @@
+<template>
+<u-info-list>
+    <u-info-list-group title="基本信息" column="3">
+        <u-info-list-item label="名称">名称</u-info-list-item>
+    </u-info-list-group>
+</u-info-list>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            instance: {},
+        };
+    },
+    created() {
+        this.$subscribe('name_PLACEHOLDER.loadDetail', (instance) => {
+            this.instance = instance;
+        });
+    },
+};
+</script>
