a7002c64a7858b66edf0ca66ad2f01f7a0fc4923
Forrest <rainforest92@126.com>
2020-07-08 02:01:08 +0800

    :bookmark: Publish @cloud-ui/s-login-page.vue@0.1.0

---
+++ src/blocks/s-login-page.vue/README.md
@@ -0,0 +1,7 @@
+# SLoginPage 登录页
+
+预置了账号与权限中心登录组件的页面。
+
+``` html
+<s-login-page></s-login-page>
+```
---
+++ src/blocks/s-login-page.vue/index.vue
@@ -0,0 +1,32 @@
+<template>
+<u-linear-layout :class="$style.main" type="flex" justify="center" alignment="center">
+    <u-linear-layout direction="vertical" justify="center">
+        <h1>登录演示</h1>
+        <s-login src="http://nuims.vusion.top" @success="onSuccess"></s-login>
+    </u-linear-layout>
+</u-linear-layout>
+</template>
+
+<script>
+import SLogin from 's-login.vue';
+
+export default {
+    components: { SLogin },
+    data() {
+        return {};
+    },
+    methods: {
+        onSuccess() {
+            location.href = '/dashboard';
+        },
+    },
+};
+</script>
+
+<style module>
+.main {
+    background: url('https://static-vusion.163yun.com/assets/login-bg-1.jpg') no-repeat;
+    background-size: cover;
+    height: 100vh;
+}
+</style>
