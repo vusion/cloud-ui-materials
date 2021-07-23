01b21c7778be75b1151cbcf481b3b9b8b4bbb011
daysai <wddpwzz36@gmail.com>
2021-07-23 17:25:34 +0800

    :bug: fix login-page templateLiteral

--- src/blocks/s-login-page.vue/index.vue
+++ src/blocks/s-login-page.vue/index.vue
@@ -2,7 +2,7 @@
 <u-linear-layout type="flex" justify="center" alignment="center" style="background: url('https://static-vusion.163yun.com/assets/login-bg-1.jpg') no-repeat; background-size: cover; height: 100vh;">
     <u-linear-layout direction="vertical" justify="center">
         <h1>登录</h1>
-        <lcap-login :src="`${protocol}//${tenant}.${nuimsDomain}`" :domain-name="domainName" @success="onSuccess"></lcap-login>
+        <lcap-login :src="src" :domain-name="domainName" @success="onSuccess"></lcap-login>
     </u-linear-layout>
 </u-linear-layout>
 </template>
@@ -10,12 +10,11 @@
 <script>
 export default {
     data() {
+        const { tenant, domainName, nuimsDomain = 'user.lcap.163yun.com' } = window.appInfo;
         return {
             // window 等变量暂时不支持变成 ASL
-            tenant: window.appInfo.tenant,
-            domainName: window.appInfo.domainName,
-            nuimsDomain: window.appInfo.nuimsDomain || 'user.lcap.163yun.com',
-            protocol: window.location.protocol,
+            domainName,
+            src: `${window.location.protocol}//${tenant}.${nuimsDomain}`,
         };
     },
     methods: {
