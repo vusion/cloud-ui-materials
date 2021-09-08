c417adbd18cdeaa4ac9f054d22a36b9d9631d393
daysai <wddpwzz36@gmail.com>
2021-09-08 11:28:09 +0800

    :sparkles: update s-login-page.vue block

--- src/blocks/s-login-page.vue/index.vue
+++ src/blocks/s-login-page.vue/index.vue
@@ -2,7 +2,7 @@
 <u-linear-layout type="flex" justify="center" alignment="center" style="background: url('https://static-vusion.163yun.com/assets/login-bg-1.jpg') no-repeat; background-size: cover; height: 100vh;">
     <u-linear-layout direction="vertical" justify="center">
         <h1>登录</h1>
-        <lcap-login :src="src" :domain-name="domainName" @success="onSuccess"></lcap-login>
+        <lcap-login :src="src" :tenant-name="tenantName" :domain-name="domainName" :env="env" @success="onSuccess"></lcap-login>
     </u-linear-layout>
 </u-linear-layout>
 </template>
@@ -14,7 +14,9 @@ export default {
         const envUri = env === 'dev' ? 'dev.' : '';
         return {
             // window 等变量暂时不支持变成 ASL
+            tenantName: tenant,
             domainName,
+            env,
             src: `${window.location.protocol}//${envUri}${tenant}.${nuimsDomain}`,
         };
     },
