502531b3e99dee21d67292ece1afff38d3fe37cd
daysai <wddpwzz36@gmail.com>
2021-09-15 16:02:11 +0800

    :bookmark: Publish lcap-login@0.4.4

--- src/components/lcap-login.vue/service.js
+++ src/components/lcap-login.vue/service.js
@@ -2,36 +2,52 @@ import axios from 'axios';
 
 const apiVersion = '2020-06-01';
 
-function preprocess(res = {}) {
-    const { status, data = {}, headers = {} } = res;
-    if (String(status).startsWith('2')) {
-        return { headers, data };
-    }
-    return Promise.reject({
-        status,
-        message: data.Message,
-    });
+function request(config) {
+    return axios(config)
+        .then((res = {}) => {
+            const { status, data = {}, headers = {} } = res;
+            if (String(status).startsWith('2')) {
+                return { headers, data };
+            }
+            return Promise.reject({
+                status,
+                message: data.Message,
+            });
+        })
+        .catch((err) => {
+            if (err.request) {
+                const { responseText, status } = err.request;
+                let message;
+                try {
+                    message = JSON.parse(responseText).Message;
+                } catch (error) {
+                    message = responseText;
+                }
+                err = { status, message };
+            }
+            throw err;
+        });
 }
 
 export default {
     getTenantLoginTypes(config) {
         const { url, params, ...rest } = config;
-        return axios({
+        return request({
             url,
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
             params: { Action: 'GetTenantLoginTypes', Version: apiVersion, ...params },
             ...rest,
-        }).then(preprocess);
+        });
     },
     login(config) {
         const { url, headers, ...rest } = config;
-        return axios({
+        return request({
             url,
             method: 'POST',
             headers: { 'Content-Type': 'application/json', ...headers },
             params: { Action: 'Login', Version: apiVersion },
             ...rest,
-        }).then(preprocess);
+        });
     },
 };
