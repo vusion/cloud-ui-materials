d7e1a82aee5965525d25ad417d87701869970869
zanningwang <wangzanning@me.com>
2022-04-19 10:32:38 +0800

    feat：封装spreadjs

--- src/components/lcap-excel/index.vue
+++ src/components/lcap-excel/index.vue
@@ -1,56 +1,77 @@
 <template>
-  <div :class="$style.root">
-    <gc-spread-sheets
-      :hostClass='$style.hostClass'
-    >
-      <gc-worksheet
-        :dataSource="dataTable"
-        :autoGenerateColumns='autoGenerateColumns'
-      >
-        <gc-column
-          :width="width"
-          :dataField="'price'"
-          :visible='visible'
-          :formatter='formatter'
-          :resizable='resizable'
-        ></gc-column>
-      </gc-worksheet>
-    </gc-spread-sheets>
+  <div id="app">
+    <gc-spread-sheets-designer :styleInfo='styleInfo' :config='config' :spreadOptions='spreadOptions' @designerInitialized='designerInitialized'>
+    </gc-spread-sheets-designer>
   </div>
 </template>
 
 <script>
-import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2016colorful.css'
-import  '@grapecity/spread-sheets-vue'
+
+import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css';
+import '@grapecity/spread-sheets-designer/styles/gc.spread.sheets.designer.min.css';
+import * as GC from '@grapecity/spread-sheets';
+import "@grapecity/spread-sheets-print";
+import "@grapecity/spread-sheets-shapes";
+import '@grapecity/spread-sheets-pivot-addon';
+import "@grapecity/spread-sheets-tablesheet";
+import '@grapecity/spread-sheets-resources-zh';
+GC.Spread.Common.CultureManager.culture("zh-cn");
+import '@grapecity/spread-sheets-designer-resources-cn';
+import "./custom.css";
+import * as ExcelIO from '@grapecity/spread-excelio';
+
+//Apply License
+//GC.Spread.Sheets.LicenseKey = 'sjs-distribution-key';
+//ExcelIO.LicenseKey = 'sjs-distribution-key';
+//GC.Spread.Sheets.Designer.LicenseKey = 'designer-component-distribution-key';
+
+import { Designer } from '@grapecity/spread-sheets-designer-vue';
+
 export default {
   name: 'lcap-excel',
-  data(){
+  data: function () {
+    var config = GC.Spread.Sheets.Designer.DefaultConfig;
+    config.commandMap = {
+      Welcome: {
+        title: "Welcome",
+        text: "Welcome",
+        iconClass: "ribbon-button-welcome",
+        bigButton: "true",
+        commandName: "Welcome",
+        execute: async (context, propertyName, fontItalicChecked) => {
+          alert("Welcome to new designer.");
+        },
+      },
+    };
+    config.ribbon[0].buttonGroups.unshift({
+      label: "NewDesigner",
+      thumbnailClass: "welcome",
+      commandGroup: {
+        children: [
+          {
+            direction: "vertical",
+            commands: ["Welcome"],
+          },
+          // This is custom button ----------------end-------------
+        ],
+      },
+    });
     return {
-      hostClass:'spread-host',
-      autoGenerateColumns:true,
-      width:300,
-      visible:true,
-      resizable:true,
-      formatter:"$ #.00"
-    }
+      styleInfo: { height: "98vh", width: '100%' },
+      config: config,
+      spreadOptions: {
+        sheetCount: 2,
+      },
+      designer: null
+    };
   },
-  computed:{
-    dataTable(){
-      let dataTable = [];
-      for (let i = 0; i < 42; i++) {
-        dataTable.push({price: i + 0.56})
-      }
-      return dataTable
+  methods: {
+    designerInitialized(value) {
+      this.designer = value;
+
     }
   }
-};
+}
 </script>
-
-<style module>
-.root {
-}
-.hostClass {
-  width: 1300px;
-  height: 600px;
-}
+<style>
 </style>
