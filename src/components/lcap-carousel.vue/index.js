c3567503e5b996ebce7af994029c673b2102d461
wanglilin0628 <hdu_lilinwang@163.com>
2022-06-24 15:49:07 +0800

    新增文字滚动条组件

---
+++ src/components/lcap-carousel.vue/index.js
@@ -0,0 +1,12 @@
+import UnnCarousel from './index.vue';
+
+export {
+    UnnCarousel
+};
+
+export default {
+    install: function(Vue) {
+        Vue.component('unn-carousel', UnnCarousel);
+    },
+    UnnCarousel
+};
