6d5e2fbed1e101dd600d7a1ccd100076f0e4487d
xuguanjie <215790428@qq.com>
2023-07-30 13:11:08 +0800

    增加移动端适配

---
+++ adapter_sdk/logics/init/index.js
@@ -0,0 +1,22 @@
+export default (options)=>{
+    // TODO
+    !function (e) {
+        var el = document.createElement("div");
+        if (/Android(?:\s+|\/)(\d+\.\d+)?/.test(navigator.userAgent)) {
+            var t = parseFloat(RegExp.$1); if (t > 2.3) {
+                console.log(t);
+                var i = parseInt(window.screen.width) / e;
+                el.innerHTML= '<meta name="viewport" content="width='+e+",minimum-scale="+i+",maximum-scale="+i+', target-densitydpi=device-dpi">'
+                document.head.appendChild(el.firstChild);
+            } else { 
+                    el.innerHTML= '<meta name="viewport" content="width=' + e + ',target-densitydpi=device-dpi">'
+                    document.head.appendChild(el.firstChild) 
+                }
+        } else { 
+            var i = parseInt(window.screen.width) / e;
+            el.innerHTML= '<meta name="viewport" content="width='+e+",minimum-scale="+i+",maximum-scale="+i+', target-densitydpi=device-dpi">'
+            document.head.appendChild(el.firstChild);
+        }
+        el=null;
+    }(375);
+}
