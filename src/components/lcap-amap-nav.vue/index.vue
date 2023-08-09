eab16803932c7ca7cb85a9fd2a09a7d2ba6e5a9c
xuguanjie <215790428@qq.com>
2023-08-09 17:13:10 +0800

    修改 导航组件

--- src/components/lcap-amap-nav.vue/index.vue
+++ src/components/lcap-amap-nav.vue/index.vue
@@ -1,7 +1,6 @@
 <template>
 <a :class="$style.root" @click="toDest" :href="currentHref" :target="target">
-    <!-- <img src="./loction.png" alt=""> -->
-    <h5-location></h5-location>
+    <img :class="$style.vaniconv" style="width: 20px;height: 20px;" src="./loction.png" alt="">
     <span :class="$style.label">{{ label }}</span>
 </a>
 </template>
@@ -53,12 +52,14 @@ export default {
 
 <style module>
 .root {
-    display: block;
+    display: flex;
     font-size: 16px;
+    align-items: center;
 }
 .vaniconv{
     font-size: 16px;
     color: #00c0cc;
+vertical-align: baseline;
 }
 .label {
     margin-left: 10px;
