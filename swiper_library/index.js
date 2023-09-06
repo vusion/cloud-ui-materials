f462845d5e41b306844fbd3b093c88414100af15
wybieshang <shangjian01@corp.netease.com>
2023-09-06 14:03:51 +0800

    feat:轮播图相关

---
+++ swiper_library/index.js
@@ -0,0 +1,8 @@
+import Gallery from './components/gallery';
+import CoverFlow from './components/cover-flow';
+// COMPONENT IMPORTS
+export {
+	Gallery,
+	CoverFlow,
+// COMPONENT EXPORTS
+};
---
+++ swiper_library/vusion.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'gallery',path: "./components/gallery/api.yaml"},,
+            { group: '组件', name: 'cover-flow',path: "./components/cover-flow/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
