cb3cfddbfa706afc2adc52da49a4a735e06750d7
xuguanjie <xuguanjie@corp.netease.com>
2023-11-08 17:44:32 +0800

    增加二维码扫一扫组件

---
+++ cw_qrcode_library/vusion.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'cw-qrcode-scan-view',path: "./components/cw-qrcode-scan-view/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
