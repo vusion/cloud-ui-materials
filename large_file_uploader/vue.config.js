39d10b412e6a523c53b50d91b5194ca2e7189922
wybieshang <1175929873@qq.com>
2023-11-22 13:46:11 +0800

    feat:基本联调完成

--- large_file_uploader/vue.config.js
+++ large_file_uploader/vue.config.js
@@ -4,21 +4,6 @@ module.exports = {
   configureWebpack: {
     module: {
       rules: [
-        // 处理CSS中的URL资源
-        // {
-        //   test: /\.css$/,
-        //   use: [
-        //     'vue-style-loader',
-        //     'css-loader',
-        //     {
-        //       loader: 'file-loader',
-        //       options: {
-        //         esModule: true,
-        //         name: 'assets/[name].[hash:8].[ext]'
-        //       }
-        //     }
-        //   ]
-        // },
         // 处理文件资源
         {
           test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
