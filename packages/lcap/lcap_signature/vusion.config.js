e8cf5f863b1f37895510d5814992c30a13e3b476
wangxue08 <wangxue08@corp.netease.com>
2023-12-12 13:43:04 +0800

    feat:更新签名板组件结构

---
+++ packages/lcap/lcap_signature/vusion.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'lcap-signature',path: "./components/lcap-signature/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
