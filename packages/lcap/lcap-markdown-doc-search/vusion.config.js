19c9555d2609c636deee97e87fce51ff636e96ef
wybieshang <1175929873@qq.com>
2023-12-12 16:13:38 +0800

    feat: 新增lcap文件夹& 迁移文档搜索器

---
+++ packages/lcap/lcap-markdown-doc-search/vusion.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'lcap-markdown-doc-search',path: "./components/lcap-markdown-doc-search/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
