5eb0535220c53dac997e78a495b82530e9f91592
wangxue08 <wangxue08@corp.netease.com>
2023-12-19 17:30:02 +0800

    feat:通用签名组件

---
+++ packages/cw/cw_signature_view/components/cw-signature-view/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="$style.root">
+    <canvas id="canvas" :width="width" :height="height"></canvas>
+  </div>
+</template>
+
+<script>
+import SmoothSignature from "smooth-signature";
+
+export default {
+  name: 'lcap-signature',
+  components: {},
+  data() {
+    return {
+      signature: null,
+      showSignatureModal: false,
+      signaturePNG: null,
+      disabled: true,
+    };
+  },
+  props: {
+    width: { type: [Number, String], default: 600 },
+    height: { type: [Number, String], default: 300 },
+    bgColor: { type: String, default: '#F8F9FA' },
+    penColor: { type: String, default: 'black' },
+    penWidth: { type: Number, default: 2 },
+    openSmooth: { type: Boolean, default: true },
+    reSignName: { type: Boolean, default: false },
+  },
+  watch: {
+    baColor() {
+      this.signature.drawBgColor();
+    },
+  },
+  mounted() {
+    this.signature = new SmoothSignature(document.getElementById("canvas"), {
+      bgColor: this.bgColor,
+      openSmooth: this.openSmooth,
+      penColor: this.penColor,
+      minWidth: this.penWidth,
+    });
+  },
+  methods: {
+    clearSignature() {
+      console.log('clear');
+      this.signature.clear();
+    },
+    getSignature() {
+      console.log('getSignature');
+      return this.signature.getPNG();
+    },
+  },
+};
+
+</script>
+
+<style module>
+.root {
+  display: inline-block;
+}
+
+</style>
