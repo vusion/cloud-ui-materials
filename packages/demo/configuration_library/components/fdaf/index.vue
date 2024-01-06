ffca321edea9444a518d5fae6d20f0020b1c20b9
xuguanjie <xuguanjie@corp.netease.com>
2024-01-06 13:58:56 +0800

    feat:应用配置的例子

---
+++ packages/demo/configuration_library/components/fdaf/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>{{value}}</div>
+</template>
+
+<script>
+export default {
+    name:"fdaf",
+    props:{
+      value:{
+        type:String,
+        default:"请在这里编写121代码"
+      }
+    },
+     mounted(){
+       fetch("/api/system/getCustomConfig/appid?group=extensions.configuration_library.custom",{
+      }).then(r=>r.text()).then((r)=>{
+          console.log(this.value=r);
+      })
+     
+    },
+    methods:{
+      getCookie(cname) {
+      const name = `${cname}=`;
+      const ca = document.cookie.split(";");
+      for (let i = 0; i < ca.length; i++) {
+        const c = ca[i].trim();
+        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+      }
+      return "";
+    },
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ packages/demo/configuration_library/vusion.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'fdaf',path: "./components/fdaf/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
