64eca69a76402332f38b9ecd2fff939345e97292
wangxue08 <wangxue08@corp.netease.com>
2024-01-12 17:45:59 +0800

    feat:音频播放依赖库

---
+++ packages/cw/cw_audio_library/components/cw-audio-view/docs/examples.md
@@ -0,0 +1,5 @@
+### 基本用法
+
+``` html
+<cw-audio-view></cw-audio-view>
+```
---
+++ packages/cw/cw_audio_library/vusion.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'cw-audio-view',path: "./components/cw-audio-view/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
