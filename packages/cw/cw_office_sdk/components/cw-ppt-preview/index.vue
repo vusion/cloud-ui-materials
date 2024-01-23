fb1bc9484db80f503ac05f39e0fa527bb6d1703a
ncqwer <1732031236@qq.com>
2024-01-23 17:37:22 +0800

    Task-2781349007512320: add preview feature for new filetype: ppt, xlsx
    * feat: add cw-ppt-preivew
    * feat: add cw-xlsx-preview
    * fix:  fix resize function error in cw-echarts-basic

---
+++ packages/cw/cw_office_sdk/components/cw-ppt-preview/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div :class="$style.root">
+        <iframe
+            :src="currentPreviewSrc"
+            :class="$style.content"
+            frameborder="0"
+            v-if="currentPreviewSrc"></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'cw-ppt-preview',
+    props: {
+        value: {
+            type: String,
+        },
+    },
+
+    computed: {
+        currentPreviewSrc() {
+            if (!this.value) return;
+            return `https://view.officeapps.live.com/op/view.aspx?src=${encodeURIComponent(
+                this.value
+            )}`;
+        },
+    },
+};
+</script>
+
+<style module>
+.root {
+    height: 500px;
+}
+
+.content {
+    width: 100%;
+    height: 100%;
+}
+</style>
