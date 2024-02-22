e5a6dd77c4f3e16eae7ea806bc1aff2f3e85236f
wybieshang <40905133+wybieshang@users.noreply.github.com>
2024-02-22 09:56:16 +0800

    Feature/swiper image (#54)
    * feat: 基础轮播图初始化
    
    * feat: 基础轮播图
    
    * feat: 插槽支持
    
    * Task-2786131023356672: slot
    
    * Task-2786131023356672: slot
    
    * Task-2786131023356672: add navigator slot

--- packages/cw/cw_swiper_library/index.js
+++ packages/cw/cw_swiper_library/index.js
@@ -1,8 +1,10 @@
 import Gallery from './components/gallery';
 import CoverFlow from './components/cover-flow';
+import BasicSwiper from './components/basic-swiper';
 // COMPONENT IMPORTS
 export {
 	Gallery,
 	CoverFlow,
+	BasicSwiper,
 // COMPONENT EXPORTS
 };
--- packages/cw/cw_swiper_library/vusion.config.js
+++ packages/cw/cw_swiper_library/vusion.config.js
@@ -4,6 +4,7 @@ module.exports = {
         components: [
             { group: '组件', name: 'gallery',path: "./components/gallery/api.yaml"},,
             { group: '组件', name: 'cover-flow',path: "./components/cover-flow/api.yaml"},,
+            { group: '组件', name: 'basic-swiper',path: "./components/basic-swiper/api.yaml"},,
             // Conponents Route List
         ],
     },
