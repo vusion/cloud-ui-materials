0522fcb549465209caafbb3d3ba5e888917135a6
ncqwer <1732031236@qq.com>
2024-02-26 10:35:09 +0800

    invalid test 2

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -18,13 +18,7 @@ const main = async () => {
     await writeChangeLog(commitInfo, taskID, isCI, isComment, noCommit);
 };
 
-const writeChangeLog = async (
-    info,
-    taskID,
-    isCI = false,
-    isComment = false,
-    noCommit = false
-) => {
+const writeChangeLog = async (info, taskID, isCI = false, isComment = false, noCommit = false) => {
     const rawInfos = await Promise.all(
         Object.entries(info).map(async ([libraryName, libraryInfo]) => {
             const targetPackagesDir = await isPackageExist(libraryName);
@@ -38,20 +32,11 @@ const writeChangeLog = async (
             const version = JSON.parse(packageVersionContent).version;
             let ans = '';
             ans += `## ${version}\n\n`;
-            ans += `Associated Task: [#${taskID.slice(
-                0,
-                6
-            )}](${taskURL}${taskID})\n\n`;
+            ans += `Associated Task: [#${taskID.slice(0, 6)}](${taskURL}${taskID})\n\n`;
             ans += genChangeLog(libraryInfo, taskID);
             const currentAdded = ans;
-            console.log(
-                '🚀 ~ file: changelog.js:67 ~ Object.entries ~ isCI:',
-                isCI
-            );
-            console.log(
-                '🚀 ~ file: changelog.js:67 ~ Object.entries ~ isComment:',
-                isComment
-            );
+            console.log('🚀 ~ file: changelog.js:67 ~ Object.entries ~ isCI:', isCI);
+            console.log('🚀 ~ file: changelog.js:67 ~ Object.entries ~ isComment:', isComment);
             if (isCI) return [libraryName, currentAdded];
             let content = '';
             try {
@@ -64,9 +49,7 @@ const writeChangeLog = async (
             const trimConent = content.trimStart();
 
             if (trimConent.startsWith(`## ${version}`)) {
-                const index = trimConent
-                    .slice(`## ${version}`.length)
-                    .search(/(?<!\#)\#\#(?!#)/);
+                const index = trimConent.slice(`## ${version}`.length).search(/(?<!\#)\#\#(?!#)/);
                 if (~index) {
                     ans += trimConent.slice(`## ${version}`.length + index);
                 }
@@ -87,6 +70,7 @@ const writeChangeLog = async (
         ans += '\n\n';
     });
     if (isCI) {
+        console.log('is message:\n', ans);
         await updatePullRequest(ans);
     } else if (isComment) {
         await tryAmendCommit(true);
@@ -99,5 +83,5 @@ const writeChangeLog = async (
 // throw new Error('manualy error');
 main().catch((e) => {
     // eslint-disable-next-line no-console
-    console.error(e.message);
+    console.error(e);
 });
