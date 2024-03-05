01bb398786c924da673f1063406a062cde977ce7
wybieshang <1175929873@qq.com>
2024-03-05 16:27:37 +0800

    feat:img-height-origin

--- packages/cw/cw_text_power_library/components/cw-wang-editor/index.vue
+++ packages/cw/cw_text_power_library/components/cw-wang-editor/index.vue
@@ -270,20 +270,13 @@ export default {
 .border {
   border: 1px solid #ccc;
 }
-.w-e-text-container img {
-    min-height: unset !important;
-    min-width: unset !important;
-    display: block;
-    height: 50px;
-    width: 50px;
-}
 </style>
 <style>
 .w-e-text-container img {
     min-height: unset !important;
     min-width: unset !important;
     display: block;
-    height: 20px;
-    width: 20px;
+    height: fit-content;
+    width: fit-content;
 }
 </style>
