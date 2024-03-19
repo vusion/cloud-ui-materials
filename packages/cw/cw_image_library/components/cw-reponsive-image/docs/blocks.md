9c61f66db44a019f214ddbc8dea4f29332fca77c
wybieshang <1175929873@qq.com>
2024-03-19 16:18:47 +0800

    feat:primary

---
+++ packages/cw/cw_image_library/components/cw-reponsive-image/docs/blocks.md
@@ -0,0 +1,5 @@
+### 基本用法
+
+``` html
+<cw-reponsive-image></cw-reponsive-image>
+```
---
+++ packages/cw/cw_image_library/vusion.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'cw-reponsive-image',path: "./components/cw-reponsive-image/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
