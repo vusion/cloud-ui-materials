2a2acd1f80731f2220504c58fded1028dd88460c
姜令浩 <jianglinghao@corp.netease.com>
2024-04-08 14:11:04 +0800

    feat: add cust_anno_permission

---
+++ packages/cust/cust_anno_permission/vusion.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'cotest',path: "./components/cotest/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
