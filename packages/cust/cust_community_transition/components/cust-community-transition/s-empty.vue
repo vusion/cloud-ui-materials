2db69a4fae4bb0490fb81b3f7987ea5ef25f5824
ncqwer <1732031236@qq.com>
2024-04-12 19:43:17 +0800

    feat: 包装transition-group

---
+++ packages/cust/cust_community_transition/components/cust-community-transition/s-empty.vue
@@ -0,0 +1,35 @@
+<template>
+    <div :class="$style.empty" s-empty="true"></div>
+</template>
+<script>
+export default {
+    name: 's-empty',
+    props: {},
+};
+</script>
+<style module>
+.empty {
+    background: #f7f8fa;
+    border: 1px dashed #c3c3c3;
+    text-align: center;
+    color: #999;
+    min-height: 30px;
+    min-width: 90px;
+    width: 100%;
+    align-items: center;
+    display: inline-flex;
+    justify-content: center;
+}
+
+.empty[inline] {
+    width: auto;
+}
+
+.empty::after {
+    content: '+';
+    font-size: 20px;
+    line-height: 12px;
+    display: inline-block;
+    margin-bottom: 2px;
+}
+</style>
