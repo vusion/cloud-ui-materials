fb690993513d64c4d4c8f17e0574b5788127cc83
ncqwer <1732031236@qq.com>
2024-05-10 15:15:46 +0800

    feat: support for ignore paper's width/height

--- packages/cw/cw_office_sdk/components/cw-doc-preview/index.vue
+++ packages/cw/cw_office_sdk/components/cw-doc-preview/index.vue
@@ -1,36 +1,47 @@
 <template>
-  <div style="display: flex;width: 100%;" :class="[$style.docPreview,$env.VUE_APP_DESIGNER&&$style.room]" ref="doc-preview">
-    <div>无效链接</div>
-  </div>
+    <div
+        style="display: flex; width: 100%"
+        :class="[$style.docPreview, $env.VUE_APP_DESIGNER && $style.room]"
+        ref="doc-preview">
+        <div>无效链接</div>
+    </div>
 </template>
 
 <script>
-import { renderAsync } from 'docx-preview'
+import { renderAsync } from 'docx-preview';
 export default {
-    name:"cw-doc-preview",
-    props:{
-      value:{
-        type:String,
-        default:"请在这里编写代码"
-      }
-  },
-  watch: {
-    value: {
-      handler: function (val, oldVal) {
-        console.log(val, oldVal)
-        this.getData(val)
-      },
-      // immediate: true
-    }
-  },
-  data() {
-    return {
-      // url:"http://static.shanhuxueyuan.com/test6.docx",
-      docxOptions: {
-                className: "kaimo-docx", // string：默认和文档样式类的类名/前缀
-                inWrapper:  true, // boolean：启用围绕文档内容的包装器渲染
-                ignoreWidth: false, // boolean：禁用页面的渲染宽度
-                ignoreHeight: false, // boolean：禁止渲染页面高度
+    name: 'cw-doc-preview',
+    props: {
+        value: {
+            type: String,
+            default: '请在这里编写代码',
+        },
+        ignoreHeight: {
+            type: Boolean,
+            default: false,
+        },
+        ignoreWidth: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    watch: {
+        value: {
+            handler: function (val, oldVal) {
+                console.log(val, oldVal);
+                this.getData(val);
+            },
+            // immediate: true
+        },
+    },
+    data() {
+        return {
+            // url:"http://static.shanhuxueyuan.com/test6.docx",
+            docxOptions: {
+                className: 'kaimo-docx', // string：默认和文档样式类的类名/前缀
+                inWrapper: true, // boolean：启用围绕文档内容的包装器渲染
+                ignoreWidth: this.ignoreWidth, // boolean：禁用页面的渲染宽度
+                ignoreHeight: this.ignoreHeight, // boolean：禁止渲染页面高度
                 ignoreFonts: false, // boolean：禁用字体渲染
                 breakPages: true, // boolean：在分页符上启用分页
                 ignoreLastRenderedPageBreak: true, // boolean：在 lastRenderedPageBreak 元素上禁用分页
@@ -41,59 +52,63 @@ export default {
                 showChanges: false, // boolean：启用文档更改的实验性渲染（插入/删除）
                 debug: false, // boolean：启用额外的日志记录
             },
-            htmlData: ''
-      }
-  },
-  mounted() {
-    this.getData(this.value.trim())
-  },
-  methods: {
-    async getData(url) {
-      if (typeof url === 'string') {
-        let buf = await fetch(url).then(r => r.arrayBuffer());
-        this.docxRender(buf);
-      }
-    },  
-    docxRender(buffer) {
-      let bodyContainer  = this.$refs["doc-preview"] 
-            
+            htmlData: '',
+        };
+    },
+    mounted() {
+        this.getData(this.value.trim());
+    },
+    methods: {
+        async getData(url) {
+            if (typeof url === 'string') {
+                let buf = await fetch(url).then((r) => r.arrayBuffer());
+                this.docxRender(buf);
+            }
+        },
+        docxRender(buffer) {
+            let bodyContainer = this.$refs['doc-preview'];
+
             renderAsync(
                 buffer, // Blob | ArrayBuffer | Uint8Array, 可以是 JSZip.loadAsync 支持的任何类型
                 bodyContainer, // HTMLElement 渲染文档内容的元素,
                 null, // HTMLElement, 用于呈现文档样式、数字、字体的元素。如果为 null，则将使用 bodyContainer。
                 this.docxOptions // 配置
-            ).then(res => {
-                console.log("res---->", res)
-            })
-    },
-    handlePreview() {
-            let file = document.getElementById("file").files[0];
+            ).then((res) => {
+                console.log('res---->', res);
+            });
+        },
+        handlePreview() {
+            let file = document.getElementById('file').files[0];
             console.log(file);
             // 将file转为buffer
             let fr = new FileReader();
             fr.readAsArrayBuffer(file);
-            fr.addEventListener("loadend",(e) => {
-                console.log("loadend---->", e)
-                let buffer = e.target.result;
-                this.docxRender(buffer);
-            },false);
+            fr.addEventListener(
+                'loadend',
+                (e) => {
+                    console.log('loadend---->', e);
+                    let buffer = e.target.result;
+                    this.docxRender(buffer);
+                },
+                false
+            );
         },
-  }
-}
+    },
+};
 </script>
 
 <style module>
-.docPreview{
-  overflow: auto;
+.docPreview {
+    overflow: auto;
 }
-.room{
-  height: 300px;
-  overflow: hidden;
+.room {
+    height: 300px;
+    overflow: hidden;
 }
 </style>
 
 <style>
-.kaimo-docx-wrapper{
-  padding:0 !important;
+.kaimo-docx-wrapper {
+    padding: 0 !important;
 }
 </style>
