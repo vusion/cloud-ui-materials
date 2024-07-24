1eb362f6ba0fcb4f9fe864110a93e81f8841dd3e
TheCoder <406132631@qq.com>
2024-07-24 19:09:21 +0800

    feat:pdf预览支持缩放

--- packages/cw/cw_office_sdk/components/cw-pdf-preview/index.vue
+++ packages/cw/cw_office_sdk/components/cw-pdf-preview/index.vue
@@ -1,91 +1,70 @@
 <template>
-    <div style="display: flex;flex-direction: column;" ref="pdf-preview" :class="$env.VUE_APP_DESIGNER&&$style.room">
-    </div>
+  <div :class="{
+    [$style.container]: true,
+    [$style.room]: $env.VUE_APP_DESIGNER
+  }">
+    <vue-pdf-embed :source="value" />
+  </div>
 </template>
 
-<script>  
-import readAsPDF from "@/utils/readAsPDF";
+<script>
+import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed';
+import panzoom from 'panzoom';
 
 export default {
-    name:"cw-pdf-preview",
-    props:{
-        value:{
-          type:String,
-        }
-  },
-  data() {
-    return {
-    }
-  },
-
-  watch:{
+  name: "cw-pdf-preview",
+  props: {
     value: {
-      handler(newVal, oldVal) {
-        console.log(newVal);
-        if (newVal) {
-          this.init()
-        }
-      },
-    }
+      type: String,
+    },
+    panZoom: {
+      type: Boolean,
+      default: false,
+    },
+    maxScale: {
+      type: Number,
+      default: 3,
+    },
+    minScale: {
+      type: Number,
+      default: 1,
+    },
   },
-  async  mounted() {
-    this.init()
+  components: {
+    VuePdfEmbed,
   },
-  methods: {
-    copyCanvasData(target, source) {
-        // console.log(target)
-        return  new Promise((resolve, reject) => {
-          const data = source.toDataURL("image/png")
-          console.log(data);
-          const img = new Image()
-          img.src = data
-          img.onload = () => {
-            const ctx = target.getContext("2d")
-            ctx.drawImage(img, 0, 0,100,100)
-            resolve(true)
-          }
-          img.onerror = () => {
-            reject(false)
-          }
-        })
-      },
-    init() {
-      try {
-        const pdfUrl = this.value;
-        if (!this.value) {
-          throw new Error("无效链接")
-        }
-        const parent = this.$refs["pdf-preview"]
-        parent.innerHTML = ''
-        readAsPDF(pdfUrl).then((pdf) => {
-          for (let i = 1; i < pdf.numPages + 1; i++){
-            pdf.getPage(i).then((page) => {
-              const viewport = page.getViewport({ scale: 2 });
-              const canvas = document.createElement('canvas');
-              parent.append(canvas);
-              const context = canvas.getContext('2d');
-              canvas.height = viewport.height;
-              canvas.width = viewport.width;
-              page.render({
-                canvasContext: context,
-                viewport: viewport
-              });
-            });
-          }
-        })
-      } catch (error) {
-        console.log(error);
-      }
-     
-    }
-  }
-}
+  mounted() {
+    this.$nextTick(() => {
+      const element = document.querySelector('.vue-pdf-embed');
+      panzoom(element, {
+        maxZoom: this.maxScale,
+        minZoom: this.minScale,
+        bounds: true,
+        boundsPadding: 0.1
+      });
+    })
+  },
+};
 </script>
 
 <style module>
-.room{
-  height: 300px;
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
   overflow: hidden;
-  border:1px solid #ccc;
+}
+
+.pinch-zoom {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+.room {
+  border: 1px dashed #ccc;
+  height: 300px;
 }
 </style>
