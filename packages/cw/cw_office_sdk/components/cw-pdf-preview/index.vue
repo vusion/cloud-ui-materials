1d46f37b507661b5060e88012cc7fd87cfb4d1b6
Yukiiii <741127583@qq.com>
2024-12-06 13:56:09 +0800

    pdf支持缩放 (#126)
    * feat:pdf预览支持缩放
    
    * fix:pdf预览组件字体丢失
    
    * fix:增加字体链接配置
    
    * feat:docx预览组件自适应
    
    * feat:天原doc文档预览自适应
    
    * feat:pdf放大后无法恢复原有尺寸
    
    ---------
    
    Co-authored-by: TheCoder <406132631@qq.com>
    Co-authored-by: wangxue08 <wangxue08@corp.netease.com>

--- packages/cw/cw_office_sdk/components/cw-pdf-preview/index.vue
+++ packages/cw/cw_office_sdk/components/cw-pdf-preview/index.vue
@@ -1,91 +1,103 @@
 <template>
-    <div style="display: flex;flex-direction: column;" ref="pdf-preview" :class="$env.VUE_APP_DESIGNER&&$style.room">
+  <div :class="{
+    [$style.container]: true,
+    [$style.room]: $env.VUE_APP_DESIGNER
+  }">
+    <div :class="$style.viewport" ref="viewport" v-if="panZoom">
+      <div :class="$style['zoomable-content']" ref="content">
+        <pinch-zoom ref="pinchZoom"  :limitPan="true" :limitZoom="maxScale" :backgroundColor="'rgb(0, 0, 0, 0)'" :minScale="minScale" >
+          <vue-pdf-embed :source="{
+            url: value,
+            cMapUrl: cMapUrl,
+            cMapPacked: true
+          }" />
+        </pinch-zoom>
+      </div>
     </div>
+    <vue-pdf-embed v-else :source="{
+      url: value,
+      cMapUrl: cMapUrl,
+      cMapPacked: true
+    }" :options="pdfOptions"/>
+  </div>
 </template>
 
-<script>  
-import readAsPDF from "@/utils/readAsPDF";
+<script>
+import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed';
+import PinchZoom from "vue-pinch-zoom";
+const pdfjsLib = require('pdfjs-dist');
+const pdfjsWorker = require('pdfjs-dist/build/pdf.worker.entry');
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 export default {
-    name:"cw-pdf-preview",
-    props:{
-        value:{
-          type:String,
-        }
+  name: "cw-pdf-preview",
+  props: {
+    value: {
+      type: String,
+    },
+    panZoom: {
+      type: Boolean,
+      default: false,
+    },
+    maxScale: {
+      type: Number,
+      default: 3,
+    },
+    minScale: {
+      type: Number,
+      default: 1,
+    },
+    cMapUrl: {
+      type: String,
+      default: '',
+    },
+  },
+  components: {
+    VuePdfEmbed,
+    PinchZoom
   },
   data() {
     return {
-    }
-  },
-
-  watch:{
-    value: {
-      handler(newVal, oldVal) {
-        console.log(newVal);
-        if (newVal) {
-          this.init()
-        }
-      },
-    }
-  },
-  async  mounted() {
-    this.init()
-  },
-  methods: {
-    copyCanvasData(target, source) {
-        // console.log(target)
-        return  new Promise((resolve, reject) => {
-          const data = source.toDataURL("image/png")
-          console.log(data);
-          const img = new Image()
-          img.src = data
-          img.onload = () => {
-            const ctx = target.getContext("2d")
-            ctx.drawImage(img, 0, 0,100,100)
-            resolve(true)
-          }
-          img.onerror = () => {
-            reject(false)
-          }
-        })
-      },
-    init() {
-      try {
-        const pdfUrl = this.value;
-        if (!this.value) {
-          throw new Error("无效链接")
-        }
-        const parent = this.$refs["pdf-preview"]
-        parent.innerHTML = ''
-        readAsPDF(pdfUrl).then((pdf) => {
-          for (let i = 1; i < pdf.numPages + 1; i++){
-            pdf.getPage(i).then((page) => {
-              const viewport = page.getViewport({ scale: 2 });
-              const canvas = document.createElement('canvas');
-              parent.append(canvas);
-              const context = canvas.getContext('2d');
-              canvas.height = viewport.height;
-              canvas.width = viewport.width;
-              page.render({
-                canvasContext: context,
-                viewport: viewport
-              });
-            });
-          }
-        })
-      } catch (error) {
-        console.log(error);
-      }
-     
-    }
+      pdfOptions: {},
+    };
   }
-}
+};
+
 </script>
 
 <style module>
-.room{
-  height: 300px;
-  overflow: hidden;
-  border:1px solid #ccc;
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  /* overflow: hidden; */
 }
+
+.viewport {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  /* overflow: hidden; */
+  touch-action: pan-y;
+  /* 禁用浏览器默认手势 */
+}
+.room {
+  border: 1px dashed #ccc;
+  height: 300px;
+}
+.zoomable-content {
+  transform-origin: top;
+}
+</style>
+
+<style>
+.vue-pdf-embed {
+  margin: auto;
+}
+
+.vue-pdf-embed canvas {
+  width: 100% !important;
+  height: auto !important;
+}
+
 </style>
