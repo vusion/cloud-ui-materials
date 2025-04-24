e07d280c67bffb20d7c64f68426dd26afa4c759f
taoyifeng <taoyifeng@corp.netease.com>
2025-04-24 18:23:24 +0800

    feat: update encrypt

--- packages/cw/cw_cryptojs_library/logics/encryptContentByARS/index.js
+++ packages/cw/cw_cryptojs_library/logics/encryptContentByARS/index.js
@@ -1,13 +1,14 @@
 import JSEncrypt from 'jsencrypt';
 /** 
  * @param {string} content <true> 加密内容
+ * @param {string} publicKey <true> 公钥
  * @returns {string} result 
  */
 export default (content, publicKey)=>{
   if (!content || !publicKey) return '';
   // RSA 加密
   const encrypt = new JSEncrypt();
-  encryptor.setPublicKey(`-----BEGIN PUBLIC KEY-----
+  encrypt.setPublicKey(`-----BEGIN PUBLIC KEY-----
     ${publicKey}
     -----END PUBLIC KEY-----`);
   // 返回十六进制字符串
