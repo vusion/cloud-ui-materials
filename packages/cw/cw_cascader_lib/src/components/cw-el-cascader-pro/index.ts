c99954ce5cb427a3c6bcc7afdec9dd762d06aaec
wangxue08 <wangxue08@corp.netease.com>
2025-05-15 15:21:41 +0800

    chore: bump version to 1.0.1 and clean up imports
    - Updated package version from 1.0.0 to 1.0.1 in package.json.
    - Removed unused VueCompositionApi import in CwElCascaderPro component.

--- packages/cw/cw_cascader_lib/src/components/cw-el-cascader-pro/index.ts
+++ packages/cw/cw_cascader_lib/src/components/cw-el-cascader-pro/index.ts
@@ -1,9 +1,7 @@
 import { extendComponent, ElCascaderPro } from 'virtual-lcap:lcap-ui';
 import ExtendElCascaderPro from './index.vue';
-import VueCompositionApi from '@vue/composition-api';
 
 export const CwElCascaderPro = extendComponent(ElCascaderPro, ExtendElCascaderPro);
 // export const CwElFormCascaderPro = WithFormItem(CwElCascaderPro, { name: 'cw-el-form-cascader-pro', methodNames: ['reload'] });
 export default CwElCascaderPro;
 
-window.VueCompositionApi = VueCompositionApi;
