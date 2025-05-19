c731d5fd67efed03ce78f0f6fbec1e0ef7ef1151
taoyifeng <taoyifeng@corp.netease.com>
2025-05-19 18:07:42 +0800

    feat: init cw_pdf_preview

---
+++ packages/cw/cw_pdf_preview/src/components/index.ts
@@ -0,0 +1,7 @@
+import PdfPreview from './pdf-preview';
+// COMPONENT IMPORTS
+
+export {
+    PdfPreview,
+// COMPONENT EXPORTS
+};
---
+++ packages/cw/cw_pdf_preview/src/components/pdf-preview/index.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="originPDFPreview"></div>
+</template>
+<script>
+import PDFObject from 'pdfobject'; 
+export default {
+  name: 'pdf-preview',
+  props: {
+    url: {
+      type: String,
+      required: true,
+    }
+  },
+  watch: {
+    url: {
+      immediate: true,
+      handler() {
+        this.$nextTick(() => {
+          this.init();
+        });
+      }
+    }
+  },
+  methods: {
+    init() {
+      PDFObject.embed(this.url, '#originPDFPreview');
+    }
+  }
+};
+</script>
+<style module>
+#originPDFPreview {
+  width: 100%;
+}
+</style>
+<style>
+.pdfobject-container {
+  height: 100%;
+}
+.pdfobject {
+  border: 1px solid #ccc;
+}
+</style>
