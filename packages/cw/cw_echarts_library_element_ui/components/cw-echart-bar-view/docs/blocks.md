27be4054e5d33ea1e173760b0aac06e04ed0f484
zhuanghengheng <zhuanghengheng@corp.netease.com>
2025-08-08 15:34:48 +0800

    feat: 上传cw_echarts_library 改造的element_ui版

---
+++ packages/cw/cw_echarts_library_element_ui/components/cw-echart-bar-view/docs/blocks.md
@@ -0,0 +1,5 @@
+### 基本用法
+
+``` html
+<cw-echart-bar-view xAxisTitle="X轴标题" yAxisTitle="Y轴标题" title="标题" theme="theme1"></cw-echart-bar-view>
+```
---
+++ packages/cw/cw_echarts_library_element_ui/components/cw-echart-line-view/index.vue
@@ -0,0 +1,233 @@
+<template>
+    <div :class="$style.root" ref="room">
+        <div :class="$style.container">
+            <echart-line
+                v-if="!loading"
+                :axisData="axisData"
+                :customStyle="customStyle"
+                :sourceData="sourceData"
+                :formatter="formatter"
+                @startLoading="startLoading"
+                ref="echart"
+                @clickItem="$emit('clickItem', $event)"
+            ></echart-line>
+            <div v-else>
+                <img
+                    :src="require('../../assets/lineEmpty.png')"
+                    :class="$style.emptyImage"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import echartLine from '@/widgets/echartLine';
+import * as echarts from 'echarts';
+import { fakeData } from '@/utils/fakeData';
+import '../../utils/theme.js';
+import Vue from 'vue';
+
+Vue.prototype.$echarts = echarts;
+let firstFlag = true;
+export default {
+    name: 'cw-echart-line-view',
+    components: { echartLine },
+    props: {
+        dataSource: [Function, Array, Object],
+        theme: { type: String, default: 'theme1' },
+        xAxis: { type: String, default: '' },
+        yAxis: { type: String, default: '' },
+        xAxisTitle: { type: String, default: 'X轴标题' },
+        yAxisTitle: { type: String, default: 'Y轴标题' },
+        title: { type: String, default: '标题' },
+        titleFontSize: { type: Number, default: 18 },
+        titleFontStyle: { type: String, default: 'normal' },
+        allowDownload: { type: Boolean, default: true },
+        allowShowHint: { type: Boolean, default: true },
+        allowShowLegend: { type: Boolean, default: true },
+        axisSplitLineType: { type: String, default: 'solid' },
+        axisSplitLine: { type: String, default: 'horizontal' },
+        areaFilled: { type: Boolean, default: false },
+        lineType: { type: String, default: 'solid' },
+        lineStyleSmooth: { type: String, default: 'normal' },
+        lineStyleSymbol: { type: String, default: 'emptyCircle' },
+        lineStyleSymbolSize: { type: String, default: '6' },
+        labelPosition: { type: String, default: 'top' },
+        undefinedToZero: { type: String, default: 'empty' },
+        legendName: { type: String, default: '' },
+        showXAxisLine: { type: Boolean, default: true },
+        showYAxisLine: { type: Boolean, default: true },
+        showXAxisLabel: { type: Boolean, default: true },
+        showYAxisLabel: { type: Boolean, default: true },
+        xAxisLabelRotate: { type: String, default: '0' },
+        initialLoad: { type: Boolean, default: true },
+        formatter: String,
+    },
+    data() {
+        return {
+            sourceData: undefined,
+            loading: false,
+            customStyle: {},
+        };
+    },
+    created() {
+        if (this.$env.VUE_APP_DESIGNER || !window.appInfo) {
+            this.init();
+        } else {
+            if (firstFlag && !this.initialLoad) {
+                this.startLoading();
+            } else {
+                this.init();
+            }
+            setTimeout(() => {
+                firstFlag = false;
+            }, 1000);
+        }
+    },
+    mounted() {
+        // 监听style样式变化
+        this.customStyle = this.parseCustomStyle(this.$el);
+        const observer = new MutationObserver(
+            function(mutations) {
+                mutations.map(
+                    function(mutation) {
+                        if (mutation.type === 'attributes') {
+                            this.customStyle = this.parseCustomStyle(this.$el);
+                        }
+                    }.bind(this)
+                );
+            }.bind(this)
+        );
+        observer.observe(this.$el, { attributes: true });
+        this.mutationObserver = observer;
+        this.resizeObserver = new ResizeObserver(() => {
+            this.$refs.echart.resize();
+        });
+        this.resizeObserver.observe(this.$el);
+    },
+    beforeDestroy() {
+        if (this.mutationObserver) {
+            this.mutationObserver.disconnect();
+        }
+        if (this.resizeObserver) {
+            this.resizeObserver.disconnect();
+        }
+    },
+    computed: {
+        axisData() {
+            return {
+                xAxis: this.xAxis,
+                yAxis: this.yAxis,
+                xAxisTitle: this.xAxisTitle,
+                yAxisTitle: this.yAxisTitle,
+                theme: this.theme,
+                title: this.title,
+                legendName: this.legendName,
+                titleFontSize: this.titleFontSize,
+                titleFontStyle: this.titleFontStyle,
+                allowDownload: this.allowDownload,
+                allowShowHint: this.allowShowHint,
+                allowShowLegend: this.allowShowLegend,
+                axisSplitLineType: this.axisSplitLineType,
+                axisSplitLine: this.axisSplitLine,
+                areaFilled: this.areaFilled,
+                lineType: this.lineType,
+                lineStyleSymbol: this.lineStyleSymbol,
+                lineStyleSmooth: this.lineStyleSmooth,
+                lineStyleSymbolSize: this.lineStyleSymbolSize,
+                labelPosition: this.labelPosition,
+                undefinedToZero: this.undefinedToZero,
+                showXAxisLine: this.showXAxisLine,
+                showYAxisLine: this.showYAxisLine,
+                showXAxisLabel: this.showXAxisLabel,
+                showYAxisLabel: this.showYAxisLabel,
+                xAxisLabelRotate: this.xAxisLabelRotate,
+            };
+        },
+        changedObj() {
+            let { xAxis, yAxis } = this;
+            return { xAxis, yAxis };
+        },
+    },
+    watch: {
+        changedObj: {
+            handler() {
+                this.init();
+            },
+        },
+    },
+    methods: {
+        parseCustomStyle(element) {
+            const cssList = element.style.cssText.split(';');
+            const cssObj = {};
+            cssList.forEach((item) => {
+                const [key, value] = item.split(':');
+                if (key && value) {
+                    cssObj[key.trim()] = value.trim();
+                }
+            });
+            return cssObj;
+        },
+        reload() {
+            this.sourceData = null
+            this.$nextTick(async () => {
+                this.sourceData = await this.handleDataSource(this.dataSource);
+                this.loading = false;
+                this.$refs.echart && this.$refs.echart.reload();
+                // console.log('source', this.sourceData);
+            });
+        },
+        async init() {
+            // 本地启动和开发环境使用假数据，生产环境替换为真数据
+            const fnDataSource =
+                this.$env.VUE_APP_DESIGNER || !window.appInfo
+                    ? fakeData
+                    : this.dataSource;
+            // console.log('fnDataSource', fnDataSource);
+            this.sourceData = await this.handleDataSource(fnDataSource);
+        },
+        async handleDataSource(dataSource) {
+            if (!dataSource) {
+                return [];
+            }
+            if (
+                dataSource instanceof Promise ||
+                typeof dataSource === 'function'
+            ) {
+                const result = await dataSource();
+                return this.getData(result);
+            }
+            return this.getData(dataSource);
+        },
+        getData(dataSource) {
+            if (typeof dataSource === 'string') {
+                dataSource = dataSource.replace(/'/g, '"');
+                return JSON.parse(dataSource);
+            }
+            return dataSource;
+        },
+        startLoading() {
+            this.loading = true;
+        },
+    },
+};
+</script>
+
+<style module>
+.root {
+    display: inline-block;
+    width: var(--echart-width, 340px);
+    height: var(--echart-height, 300px);
+}
+
+.container {
+    width: 100%;
+    height: 100%;
+}
+
+.emptyImage {
+    width: 100%;
+    height: 100%;
+}
+</style>
